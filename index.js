c19d59dbbb776d9b81accb45ce6ec6dc4e17e4c6
Rohan Prakash <rohan.prakash.2.12@gmail.com>
2020-11-27 20:26:49 +0000

    alignment

--- index.js
+++ index.js
@@ -2,42 +2,58 @@ var radLineSync = require("readline-sync");
 
 var score = 0;
 
+console.log(`How well do you know Rohan `);
+console.log(`****************************`)
+
 var game = (question,answer) => {
   var playerAnswer = radLineSync.question(question);
   if(playerAnswer.toUpperCase() === answer.toUpperCase()){
-    console.log("Well done, right answer");
+    console.log(`
+Well done, right answer
+      `);
     score++
   } else {
-    console.log("Sorry, that was wrong");
+    console.log(`
+Sorry, that was wrong`);
   }
   console.log("Your current score is : " + score);
   console.log(`
-  **********************************`)
+**********************************`)
 }
 
 var questionSet = [{
   question : `
-  Rohan's hometown is ______ `,
+Rohan's hometown is ______ 
+
+Your Answer : `,
   answer : "Kanpur"
 },
 {
   question : `
-  Rohan is _______ years old `,
+Rohan is _______ years old 
+
+Your Answer : `,
   answer : "21"
 },
 {
   question : `
-  Rohan's favourite dish is _________`,
+Rohan's favourite dish is _________
+
+Your Answer : `,
   answer : "Butter Chicken"
 },
 {
   question : `
-  Rohan is a _____________ by profession`,
+Rohan is a _____________ by profession
+
+Your Answer : `,
   answer : "Developer"
 },
 {
   question :`
-  Rohan's pet name is ______`,
+Rohan's pet name is ______
+
+Your Answer : `,
   answer : "Leo"
 }];
 
